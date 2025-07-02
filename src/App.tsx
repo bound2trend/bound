445b33056aec4ebfed03809402db714f237d6c68
bound2trend <bound2trend@gmail.com>
2025-07-02 20:49:07 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,47 @@
-import React from 'react';
+// src/App.tsx
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// Layout
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
+import ScrollToTop from './components/common/ScrollToTop';
+import LoadingSpinner from './components/common/LoadingSpinner';
 
-// Pages
-import HomePage from './pages/HomePage';
-import ShopPage from './pages/ShopPage';
-import ProductDetailPage from './pages/ProductDetailPage';
-import CartPage from './pages/CartPage';
-import CheckoutPage from './pages/CheckoutPage';
-import WishlistPage from './pages/WishlistPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import AccountPage from './pages/AccountPage';
-import ContactPage from './pages/ContactPage';
-import FaqPage from './pages/FaqPage';
-import NotFoundPage from './pages/NotFoundPage';
-
-// UI Modals/Prompts
-import NewsletterModal from './components/home/NewsletterModal';
-import BackInStockPrompt from './components/home/BackInStockPrompt';
+// Lazy-loaded pages
+const HomePage = React.lazy(() => import('./pages/HomePage'));
+const ShopPage = React.lazy(() => import('./pages/ShopPage'));
+const ProductDetailPage = React.lazy(() => import('./pages/ProductDetailPage'));
+const CartPage = React.lazy(() => import('./pages/CartPage'));
+const CheckoutPage = React.lazy(() => import('./pages/CheckoutPage'));
+const WishlistPage = React.lazy(() => import('./pages/WishlistPage'));
+const LoginPage = React.lazy(() => import('./pages/LoginPage'));
+const RegisterPage = React.lazy(() => import('./pages/RegisterPage'));
+const AccountPage = React.lazy(() => import('./pages/AccountPage'));
+const ContactPage = React.lazy(() => import('./pages/ContactPage'));
+const FaqPage = React.lazy(() => import('./pages/FaqPage'));
+const NotFoundPage = React.lazy(() => import('./pages/NotFoundPage'));
 
 const App: React.FC = () => {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen font-sans text-neutral-900 bg-white">
-        <Header />
-        
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/shop" element={<ShopPage />} />
-            <Route path="/product/:id" element={<ProductDetailPage />} />
-            <Route path="/cart" element={<CartPage />} />
-            <Route path="/checkout" element={<CheckoutPage />} />
-            <Route path="/wishlist" element={<WishlistPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/account" element={<AccountPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/faq" element={<FaqPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </main>
-
-        <Footer />
-
-        {/* Global Modals */}
-        <NewsletterModal />
-        <BackInStockPrompt />
-      </div>
+      <ScrollToTop />
+      <Header />
+      <Suspense fallback={<LoadingSpinner />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/shop" element={<ShopPage />} />
+          <Route path="/product/:id" element={<ProductDetailPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/wishlist" element={<WishlistPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/account" element={<AccountPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/faq" element={<FaqPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+      <Footer />
     </Router>
   );
 };
